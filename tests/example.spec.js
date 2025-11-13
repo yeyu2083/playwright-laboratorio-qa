fc3f75f536f3c9a90a21109394a6c2417074a538
yeyu2083 <ywindecker@gmail.com>
2025-11-13 20:08:53 -0300

    agregamos un nuevo test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,14 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('playwright.dev tiene Playwright en el título', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('Ambassadors incluye a Carlos Gauto de Argentina', async ({ page }) => {
+  await page.goto('https://playwright.dev/community/ambassadors');
+  // Espera a que el nombre Carlos Gauto esté visible
+  await expect(page.getByText('Carlos Gauto')).toBeVisible();
+  // Verifica que la ubicación sea Argentina
+  await expect(page.getByText('Argentina')).toBeVisible();
 });
